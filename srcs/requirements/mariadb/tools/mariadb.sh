08a77ce09eae4ec20f5709169dc6163456e10565
joppe <joppe@pop-os.localdomain>
2024-06-13 21:55:21 +0200

    Wordpress thingy works with mariadb

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
 
-#---------------------------------------------------zsh---------------------------------------------------#
-
-chsh -s $(which zsh)
-wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
-echo "alias zshi='sh /install.sh'" >> ~/.zshrc
-
-#---------------------------------------------------mariadb start---------------------------------------------------#
 
 service mariadb start
 sleep 5
 
-#---------------------------------------------------mariadb config---------------------------------------------------#
 
-mariadb -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DB}\`;"
+mariadb -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
 mariadb -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-mariadb -e "GRANT ALL PRIVILEGES ON ${MYSQL_DB}.* TO \`${MYSQL_USER}\`@'%';"
+mariadb -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO \`${MYSQL_USER}\`@'%';"
 mariadb -e "FLUSH PRIVILEGES;"
 
 #---------------------------------------------------mariadb restart---------------------------------------------------#
